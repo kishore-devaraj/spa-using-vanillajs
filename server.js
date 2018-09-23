169b8e686faa1279473945a3c89778bd0dd09ac8
kishore-devaraj <kishoregrylls@gmail.com>
2018-09-23 23:48:27 +0530

    Added CORS middleware to the proxy nodejs server

--- server.js
+++ server.js
@@ -9,6 +9,15 @@ const app = express()
 app.use(express.static('public'))
 app.use('/scripts', express.static(`${__dirname}/node_modules/`))
 
+// CORS middleware
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
+
 // Parse POST data as URL encoded data
 app.use(bodyParser.urlencoded({
     extended: true,
